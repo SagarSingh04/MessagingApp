3df94ab6627d4452f1810ef078458e13b5a4505e
Sagar Singh <sagar.a.singh04@gmail.com>
2022-12-27 14:34:57 +0530

    cors added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const usersRoute = require('./api/routes/users');
 const groupsRoute = require('./api/routes/groups');
@@ -11,6 +12,7 @@ const analyticsRoute = require('./api/routes/analytics');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/users', usersRoute);
 app.use('/groups', groupsRoute);
